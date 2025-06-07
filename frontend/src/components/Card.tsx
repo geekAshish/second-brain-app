0997a0a414d58744101ac37c31dcf269bb213df1
Ashish Kushwaha <ashish@wishfy.club>
2025-06-07 15:39:00 +0530

    toggle share

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -109,21 +109,23 @@ export function Card({
       </Modal>
 
       <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border min-h-48 min-w-72">
-        <div className="flex justify-end items-end">
-          <label className="inline-flex items-center justify-end cursor-pointer">
-            <span className="mr-1 text-sm font-medium text-gray-900 dark:text-gray-500">
-              Share with others
-            </span>
+        {contentId && (
+          <div className="flex justify-end items-end">
+            <label className="inline-flex items-center justify-end cursor-pointer">
+              <span className="mr-1 text-sm font-medium text-gray-900 dark:text-gray-500">
+                Share with others
+              </span>
 
-            <input
-              type="checkbox"
-              value=""
-              className="sr-only peer"
-              onChange={shareToOtherHandler}
-            />
-            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-500 dark:peer-checked:bg-purple-600"></div>
-          </label>
-        </div>
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                onChange={shareToOtherHandler}
+              />
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-500 dark:peer-checked:bg-purple-600"></div>
+            </label>
+          </div>
+        )}
 
         <div className="flex justify-between">
           <div className="flex items-center text-md">
